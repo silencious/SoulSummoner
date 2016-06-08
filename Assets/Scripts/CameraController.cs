bee24aab812a0cf1cfe34c6643dff41c8e3c79dc
Silencious <tjh0473@gmail.com>
2016-06-08 22:40:57 +0800

    basic file structure

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform lookAtObj;
+
+	private float maxDis = 50.0f;
+	private float minDis = 3.0f;
+	private float distance=10.0f;
+
+	private float maxTheta = 179.0f;
+	private float minTheta = 1.0f;
+	private float phi=0.0f;
+	private float theta=30.0f;
+
+	void Start(){
+		Vector3 vec = transform.position - lookAtObj.transform.position;
+		float factor = 180.0f / Mathf.PI;
+		theta = Mathf.Atan2(vec.x,vec.y)*factor;
+		phi = Mathf.Atan2 (vec.z, vec.x)*factor;
+		distance = vec.magnitude;
+	}
+
+	void Update(){
+		// rotate camera
+		if(Input.GetMouseButton(1)){
+			float rotateX = Input.GetAxis ("Mouse X");
+			float rotateY = Input.GetAxis ("Mouse Y");
+			phi += rotateX*2.0f;
+			theta += rotateY*2.0f;
+
+			theta = Mathf.Min (theta, maxTheta);
+			theta = Mathf.Max (theta, minTheta);
+			//Debug.Log ("phi=" + phi + " theta=" + theta);
+		}
+		Vector3 vec = Quaternion.Euler (0,phi,theta)*Vector3.up;
+		
+		// update distance
+		float scroll = Input.GetAxis ("Mouse ScrollWheel");
+		distance *= (1 - 0.6f*scroll);
+		distance = Mathf.Min (distance, maxDis);
+		distance = Mathf.Max (distance, minDis);
+		transform.position = lookAtObj.position + vec * distance;
+
+		// keep direction
+		transform.LookAt (lookAtObj);
+	}
+}
---
+++ Assets/Scripts/PCController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class PCController : MonoBehaviour
+{
+	public Animator anim;
+	public Rigidbody rb;
+	public Camera cam;
+	public float moveSpeed;
+	public float jumpSpeed;
+	public bool onGround;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody> ();
+		moveSpeed = 15.0f;
+		jumpSpeed = 10.0f;
+		onGround = true;
+	}
+
+	void Update ()
+	{
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 forward = cam.transform.forward;
+		forward.y = 0;
+
+		forward.Normalize ();
+		Vector3 rightward = Vector3.Cross (Vector3.up, forward);
+
+		Vector3 velocity = moveSpeed * (forward * vertical+rightward*horizontal);
+
+		if (Mathf.Abs(vertical)+Mathf.Abs(horizontal) > 0.1f) {
+			// set direction of character
+			transform.forward = velocity;
+		}
+
+
+		//anim.SetFloat ("velocity", velocity.magnitude);
+
+		if (Input.GetKey (KeyCode.Space) && onGround) {
+			velocity.y = jumpSpeed;
+			onGround = false;
+			//anim.Play("Jump");
+			//anim.SetBool("jumping", true);
+		} else {
+			velocity.y = rb.velocity.y;
+		}
+
+		// set velocity of rigidbody
+		rb.velocity =velocity;
+
+		if (Input.GetMouseButton (0)) {
+			//anim.Play ("Reach");
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		//Debug.Log ("collision enter");
+		if (other.gameObject.CompareTag ("Terrain")) {
+			onGround = true;
+			//anim.SetBool ("jumping", false);
+		}
+	}
+
+	void OnCollisionStay(Collision other){
+		if (other.gameObject.CompareTag ("Cube") && Input.GetMouseButton(0)) {
+			other.gameObject.SendMessage ("Explode");
+		}
+	}
+}
